e410b0fd553e88c66d44ddcc9ec9bea2a2c64506
sauceofmagic <avilcins@gmail.com>
2024-09-20 16:39:16 +0100

    counting characters in book

--- main.py
+++ main.py
@@ -6,10 +6,18 @@ def main():
     text = get_book_text(book_path)
     num_words = get_num_words(text)
     character_counts = get_num_char(text)
+    print("--- Begin report of books/frankenstein.txt ---")
     print(f"{num_words} words found in the document")
-    count_a = character_counts.get('a', 0)  # Using get to avoid KeyError if 'a' is not in the dictionary
-    print(f"Count of letters: {character_counts}")
+    
+    # Find the count of the letter 'a'
+    count_a = next((entry['num'] for entry in character_counts if entry['character'] == 'a'), 0)
 
+    print(f"Count of 'a': {count_a}")
+
+    for entry in character_counts:
+        print(f"The '{entry['character']}' character was found {entry['num']} times")
+    
+    print("--- End report ---")
 
 def get_num_words(text):
     words = text.split()
@@ -19,14 +27,23 @@ def get_num_words(text):
 def get_num_char(text):
     text = text.lower()
     char_count = {}
-
+    # Counting the characters
     for char in text:
         if char.isalpha():
             if char in char_count:
                 char_count[char] += 1
             else:
                 char_count[char] = 1
-    return char_count
+
+    # Getting characters dictionary into a list of dictionaries for sorting
+    char_list = []
+    for char, count in char_count.items():
+        char_list.append({"character": char, "num": count})
+
+    # Sorting the list of dictionaries based on num value
+    char_list.sort(key=lambda x: x['num'], reverse=True)
+    
+    return char_list
 
 
 def get_book_text(path):
